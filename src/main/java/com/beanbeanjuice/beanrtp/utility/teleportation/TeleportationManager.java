39d01f000dbd9e37b4ce72b5e6d6d4c9381b48eb
beanbeanjuice <beanbeanjuice@outlook.com>
2024-01-04 18:51:04 -0800

    Created Teleportation Cooldown

--- src/main/java/com/beanbeanjuice/beanrtp/utility/teleportation/TeleportationManager.java
+++ src/main/java/com/beanbeanjuice/beanrtp/utility/teleportation/TeleportationManager.java
@@ -1,18 +1,17 @@
 package com.beanbeanjuice.beanrtp.utility.teleportation;
 
 import com.beanbeanjuice.beanrtp.BeanRTP;
-import com.beanbeanjuice.beanrtp.utility.Helper;
+import com.beanbeanjuice.beanrtp.utility.CooldownManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.BlockingQueue;
 import java.util.logging.Level;
 import java.util.stream.Stream;
 
@@ -20,16 +19,26 @@ public class TeleportationManager {
 
     private static BeanRTP plugin;
     private static TeleportationSettings settings;
+    private static CooldownManager cooldownManager;
 
-    private static final int MAX_LOCATION_COUNT = 20;
+    private static final int MAX_LOCATIONS_PER_PLAYER = 3;
+    private static final int MIN_LOCATIONS_PER_PLAYER = 1;
 
     private static final Material[] unsafeMaterials = new Material[] {
-            Material.AIR,
             Material.LAVA,
-            Material.WATER
+            Material.WATER,
+            Material.KELP,
+            Material.KELP_PLANT,
+            Material.SEAGRASS,
+            Material.SEA_PICKLE,
+            Material.FIRE,
+            Material.MAGMA_BLOCK
     };
 
     private static final Material[] ignoredMaterials = new Material[] {
+            Material.LAVA,
+            Material.WATER,
+
             Material.AIR,
             Material.CAVE_AIR,
             Material.VOID_AIR,
@@ -42,7 +51,10 @@ public class TeleportationManager {
             Material.MANGROVE_LEAVES,
             Material.FLOWERING_AZALEA_LEAVES,
             Material.OAK_LEAVES,
-            Material.SPRUCE_LEAVES
+            Material.SPRUCE_LEAVES,
+            Material.SNOW,
+            Material.SHORT_GRASS,
+            Material.TALL_GRASS
     };
 
     private static HashMap<World, Vector<Location>> safeLocations = new HashMap<>();
@@ -53,39 +65,53 @@ public class TeleportationManager {
         plugin = beanRTP;
         settings = new TeleportationSettings(plugin);
 
+        cooldownManager = new CooldownManager(plugin.getConfig().getInt("cooldown-time"));
+
         plugin.getLogger().log(Level.INFO, "Populating RTP locations...");
         populateLocations(settings.getAllowedWorlds());
     }
 
     private static void populateLocations(String[] allowedWorlds) {
+        Stream<World> worlds = Arrays.stream(allowedWorlds).map(Bukkit::getWorld);
+
+        // Using a vector to remain thread safe.
+        worlds.forEach((world) -> safeLocations.putIfAbsent(world, new Vector<>()));
+        safeLocations.forEach(TeleportationManager::createAsyncPopulationTask);
+    }
+
+    private static void createAsyncPopulationTask(World world, Vector<Location> locations) {
+        if (locations.size() >= getMinLocationsCount()) return;
+
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            Stream<World> worlds = Arrays.stream(allowedWorlds).map(Bukkit::getWorld);
-
-            worlds.forEach((world) -> safeLocations.putIfAbsent(world, new Vector<>()));
-
-            safeLocations.forEach((world, locations) -> {
-                while (locations.size() != MAX_LOCATION_COUNT) {
-                    try {
-                        locations.add(getNewLocation(world));
-                        plugin.getLogger().log(Level.INFO, "Adding RTP location for " + world + ": " + locations.size() + "/" + MAX_LOCATION_COUNT);
-                    } catch (InterruptedException ignored) { }
-                }
-            });
+            while (locations.size() < getMaxLocationsCount()) {
+                try {
+                    locations.add(getNewLocation(world));
+                    plugin.getLogger().log(
+                            Level.INFO,
+                            "Adding RTP location for %s: %d/%d".formatted(
+                                    world.getName(), locations.size(), getMaxLocationsCount()
+                            )
+                    );
+                } catch (InterruptedException ignored) { }
+            }
         });
     }
 
     public static boolean teleport(Player player) {
         World world = player.getWorld();
 
-        if (safeLocations.get(world).isEmpty()) {
-            Helper.sendMessage(player, "RTP locations are currently loading...");
-            return false;
-        }
+        if (safeLocations.get(world).isEmpty()) return false;
 
         Location location = safeLocations.get(world).removeFirst();
-        player.teleport(location);
 
-        if (safeLocations.get(world).isEmpty())
+        // Used to stop console spam of "MOVED TOO QUICKLY"
+        Bukkit.getScheduler().runTaskLater(
+                plugin,
+                () -> player.teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN),
+                1
+        );
+
+        if (safeLocations.get(world).size() <= getMinLocationsCount())
             populateLocations(settings.getAllowedWorlds());
 
         return true;
@@ -114,26 +140,110 @@ public class TeleportationManager {
             newLocation = new Location(worldBorderCenter.getWorld(), x, -256, z);
             newLocation = getTopBlock(newLocation);
             newLocation.add(0.5, 0.5, 0.5);
-            Thread.sleep(Duration.ofSeconds(5));
-        } while (worldBorderCenter.distance(newLocation) < settings.getMinimumDistanceFromBorderCenter() && !isSafe(newLocation));
+        } while (worldBorderCenter.distance(newLocation) < settings.getMinimumDistanceFromBorderCenter() || !isSafe(newLocation));
 
         return newLocation.add(0, 0.5, 0);
     }
 
     private static boolean isSafe(Location location) {
-        Material material1 = location.getBlock().getType();
-        Material material2 = location.clone().subtract(0, 1, 0).getBlock().getType();
-        Material material3 = location.clone().add(0, 1, 0).getBlock().getType();
+        // -2 must not be in unsafe or ignored.
+        // -1 must not be in unsafe.
+        // 0 must not be in unsafe.
+        // +1 must not be in unsafe.
+        // +2 must not be in unsafe.
 
         var unsafeArray = Arrays.asList(unsafeMaterials);
-        return !unsafeArray.contains(material1) &&
-                !unsafeArray.contains(material2) &&
-                !unsafeArray.contains(material3);
+        var ignoredArray = Arrays.asList(ignoredMaterials);
+
+        Material materialSub2 = location.clone().subtract(0, 2, 0).getBlock().getType();
+        if (unsafeArray.contains(materialSub2) || ignoredArray.contains(materialSub2))
+            return false;
+
+        Material materialSub1 = location.clone().subtract(0, 1, 0).getBlock().getType();
+        if (unsafeArray.contains(materialSub1))
+            return false;
+
+        Material material = location.clone().getBlock().getType();
+        if (unsafeArray.contains(material))
+            return false;
+
+        Material materialAdd1 = location.clone().add(0, 1, 0).getBlock().getType();
+        if (unsafeArray.contains(materialAdd1))
+            return false;
+
+        Material materialAdd2 = location.clone().add(0, 2, 0).getBlock(). getType();
+        if (unsafeArray.contains(materialAdd2))
+            return false;
+
+        return true;
+
     }
 
     private static Location getTopBlock(Location location) {
-        Block block = location.getWorld().getHighestBlockAt(location);
-        return block.getLocation();
+        double MAX_BUILD_HEIGHT = 320.5;
+        double MAX_BUILD_HEIGHT_NETHER = 127.5;
+        double MIN_BUILD_HEIGHT = -64.5;
+
+        if (location.getWorld().getEnvironment().equals(World.Environment.NETHER)) location.setY(MAX_BUILD_HEIGHT_NETHER);
+        else location.setY(MAX_BUILD_HEIGHT);
+
+        Stack<Material> materials = new Stack<>();
+
+        while (location.getY() >= MIN_BUILD_HEIGHT) {
+            materials.push(location.getBlock().getType());
+
+            if (checkMaterialStack(materials)) {
+                location.add(0, 1, 0);
+                return location;
+            }
+
+            location.subtract(0, 1, 0);
+        }
+
+        location.setY(-100);  // Error
+        return location;
+    }
+
+    private static boolean checkMaterialStack(Stack<Material> materials) {
+        if (materials.size() < 3) return false;
+
+        Material first = materials.pop();
+        Material second = materials.pop();
+        Material third = materials.pop();
+
+        if (Arrays.asList(ignoredMaterials).contains(first)) {
+            materials.push(third);
+            materials.push(second);
+            materials.push(first);
+            return false;
+        }
+
+        if (!(Arrays.asList(ignoredMaterials).contains(second) && Arrays.asList(ignoredMaterials).contains(third))) {
+            materials.push(third);
+            materials.push(second);
+            materials.push(first);
+            return false;
+        }
+
+        return true;
+    }
+
+    private static int getMinLocationsCount() {
+        int onlinePlayers = Bukkit.getOnlinePlayers().size();
+        if (onlinePlayers == 0) return MIN_LOCATIONS_PER_PLAYER;
+
+        return onlinePlayers * MIN_LOCATIONS_PER_PLAYER;
+    }
+
+    private static int getMaxLocationsCount() {
+        int onlinePlayers = Bukkit.getOnlinePlayers().size();
+        if (onlinePlayers == 0) return MAX_LOCATIONS_PER_PLAYER;
+
+        return onlinePlayers * MAX_LOCATIONS_PER_PLAYER;
+    }
+
+    public static CooldownManager getCooldownManager() {
+        return cooldownManager;
     }
 
 }
